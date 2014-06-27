806c5e2e910298bfa7eeccecd6ff49ac2bf4e860
gbachik <gbachik@gmail.com>
2014-06-27 13:39:51 -0600

    Added styling to jQuery

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).on('ready', function() {
-  
+	
+	$.fn.createSlidshow = function(){
+		var cssLi = {
+			'list-style' : 'none'
+		}
+		this.find('li').css(cssLi);
+		console.log('createSlidshow Called!');
+	};
+
+	$('.slideshow').createSlidshow()
+
 });
